59158a52e526a308d64e8dcdd28ca449b6dcf4f1
Damian Soto <soto.platero@gmail.com>
2025-09-17 07:43:58 -0400

    remove edit theme from edit view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div class="min-h-screen bg-base-100">
-	<div class="max-w-xl mx-auto min-h-screen flex flex-col justify-center">
+	<div class="max-w-xl mx-auto min-h-screen flex flex-col justify-center p-8">
 		<h1 class="text-5xl font-bold text-center mb-2">Speak. Post. Done.</h1>
-		<p class="text-gray-600 mb-8 text-center text-xl">Turn your voice into posts on X instantly.</p>
+		<p class="text-base-content/70 mb-8 text-center text-xl">Turn your voice into posts on X instantly.</p>
 
 		<!-- Voice Recorder Component with all functionality -->
 		<VoiceRecorder />
